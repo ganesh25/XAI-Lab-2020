d4eeeccf33628714dbfaf434bfa0a4674e81b89d
Ganesh Chandrasekaran <ge29puq@mytum.de>
2020-11-30 15:27:10 +0100

    Add integrated_gradients implementation

---
+++ xai/Integrated_Gradients.py
@@ -0,0 +1,114 @@
+import spacy
+import torch
+import torchtext
+import torchtext.data
+import torch.nn as nn
+import torch.nn.functional as F
+from torchtext.vocab import Vocab
+from captum.attr import LayerIntegratedGradients, TokenReferenceBase, visualization
+from torchtext import vocab
+from models.improved_rnn import Improved_RNN
+from models.simple_rnn import RNN, CNN
+
+nlp = spacy.load('en')
+
+device = torch.device('cuda:5' if torch.cuda.is_available() else 'cpu')
+
+# load the previously saved model
+model.load_state_dict(torch.load("./models/model_imdb.pt"))
+model.eval()
+model = model.to(device)
+
+
+def forward_with_sigmoid(input):
+    return torch.sigmoid(model(input))
+
+
+TEXT = torchtext.data.Field(lower=True, tokenize='spacy')
+Label = torchtext.data.LabelField(dtype = torch.float)
+
+
+train, test = torchtext.datasets.IMDB.splits(text_field=TEXT,
+                                      label_field=Label,
+                                      train='train',
+                                      test='test',
+                                      path='data/aclImdb')
+test, _ = test.split(split_ratio = 0.04)
+
+
+#loaded_vectors = vocab.GloVe(name='6B', dim=50)
+
+# If you prefer to use pre-downloaded glove vectors, you can load them with the following two command line
+loaded_vectors = torchtext.vocab.Vectors('data/glove.6B.50d.txt')
+TEXT.build_vocab(train, vectors=loaded_vectors, max_size=len(loaded_vectors.stoi))
+    
+TEXT.vocab.set_vectors(stoi=loaded_vectors.stoi, vectors=loaded_vectors.vectors, dim=loaded_vectors.dim)
+Label.build_vocab(train)
+
+
+PAD_IND = TEXT.vocab.stoi['pad']
+token_reference = TokenReferenceBase(reference_token_idx=PAD_IND)
+
+# create an instance of LayerIntegratedGradients
+lig = LayerIntegratedGradients(model, model.embedding)
+
+
+
+# accumalate couple samples in this array for visualization purposes
+vis_data_records_ig = []
+
+def interpret_sentence(model, sentence, min_len = 7, label = 0):
+    text = [tok.text for tok in nlp.tokenizer(sentence)]
+    if len(text) < min_len:
+        text += ['pad'] * (min_len - len(text))
+    indexed = [TEXT.vocab.stoi[t] for t in text]
+
+    model.zero_grad()
+
+    input_indices = torch.tensor(indexed, device=device)
+    input_indices = input_indices.unsqueeze(0)
+    
+    # input_indices dim: [sequence_length]
+    seq_length = min_len
+
+    # predict
+    pred = forward_with_sigmoid(input_indices).item()
+    pred_ind = round(pred)
+
+    # generate reference indices for each sample
+    reference_indices = token_reference.generate_reference(seq_length, device=device).unsqueeze(0)
+
+    # compute attributions and approximation delta using layer integrated gradients
+    attributions_ig, delta = lig.attribute(input_indices, reference_indices, \
+                                           n_steps=500, return_convergence_delta=True)
+
+    print('pred: ', Label.vocab.itos[pred_ind], '(', '%.2f'%pred, ')', ', delta: ', abs(delta))
+
+    add_attributions_to_visualizer(attributions_ig, text, pred, pred_ind, label, delta, vis_data_records_ig)
+    
+def add_attributions_to_visualizer(attributions, text, pred, pred_ind, label, delta, vis_data_records):
+    attributions = attributions.sum(dim=2).squeeze(0)
+    attributions = attributions / torch.norm(attributions)
+    attributions = attributions.cpu().detach().numpy()
+
+    # storing couple samples in an array for visualization purposes
+    vis_data_records.append(visualization.VisualizationDataRecord(
+                            attributions,
+                            pred,
+                            Label.vocab.itos[pred_ind],
+                            Label.vocab.itos[label],
+                            Label.vocab.itos[1],
+                            attributions.sum(),       
+                            text,
+                            delta))
+
+
+interpret_sentence(model, 'It was a fantastic performance !', label=1)
+interpret_sentence(model, 'Best film ever', label=1)
+interpret_sentence(model, 'It was a horrible movie', label=0)
+interpret_sentence(model, 'I have never watched such a bad movie', label=0)
+interpret_sentence(model, 'It is a disgusting movie!', label=0)
+
+
+print('Visualize attributions based on Integrated Gradients')
+visualization.visualize_text(vis_data_records_ig)
